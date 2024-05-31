cc525c4994ca9514acb3d66bd6bfef6bb357f2f3
Jeroen1602 <1492076+jeroen1602@users.noreply.github.com>
2024-05-31 16:05:34 +0200

    Added native as a benchmark.
    Added puppeteer script to automatically test and print results.

---
+++ index.js
@@ -0,0 +1,55 @@
+import puppeteer from 'puppeteer';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+const repeatCount = 10;
+
+app.use('/native', express.static('./native'));
+
+const server = app.listen(port, async () => {
+    console.log('app has started!');
+
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+    await testPage(browser, 'native', repeatCount);
+
+    await browser.close();
+    server.close();
+});
+
+
+/**
+ *
+ * @param {Browser} browser
+ * @param {string} path
+ * @param {number} repeat
+ */
+async function testPage(browser, path, repeat = 100) {
+    console.log(`Going to test path '${path}' with ${repeat} rounds`);
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${port}/${path}/`);
+    await page.setViewport({width: 1280, height: 720});
+
+    let total = 0;
+    for (let i = 0; i < repeat; i++) {
+        const time = await page.$('#time');
+
+        const startText = await time.evaluate(el => el.textContent);
+        await page.click('#start');
+
+        while ((await time.evaluate(el => el.textContent)) === startText) {
+            await new Promise(r => setTimeout(r, 100));
+        }
+        const timeTaken = parseInt(await time.evaluate(el => el.textContent));
+        total += timeTaken / repeat;
+        await page.reload();
+    }
+    console.log(`For path '${path}' average time taken was ${total} ms after ${repeat} rounds`);
+
+    await page.close();
+
+}
+
